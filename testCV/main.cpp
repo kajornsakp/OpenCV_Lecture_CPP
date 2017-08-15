c7960bd06d232360d8eba9132963d9ec55bf5c52
Kajornsak Peerapathananont <kajornsak_psm@hotmail.com>
2017-08-15 23:06:50 +0700

    Week 2 : Assignment

--- testCV/main.cpp
+++ testCV/main.cpp
@@ -19,12 +19,29 @@ int main( int argc, const char** argv )
 {
     std::cout << CV_VERSION << endl;
     Mat image;
-    image = imread("<#image path#>",CV_LOAD_IMAGE_COLOR);
+    image = imread("a.png",CV_LOAD_IMAGE_COLOR);
+    cv::resize(image, image, cv::Size(0,0),0.2,0.2);
     if(!image.data){
         cout << "no image" << endl;
         return -1;
     }
     namedWindow("Window",WINDOW_AUTOSIZE);
+    std::cout << image.size << endl;
+//    imshow("Window", image);
+    //Vec3b b,g,r
+    for (int i = 0; i < image.rows; ++i) {
+        for (int j = 0; j < image.cols; ++j) {
+            if(i < image.rows/2 && j > image.cols/2){
+                image.at<cv::Vec3b>(i,j) = Vec3b(image.at<cv::Vec3b>(i,j)[1],0,0);
+            }
+            else if(i > image.rows/2 && j < image.cols/2){
+                image.at<cv::Vec3b>(i,j) = Vec3b(0,image.at<cv::Vec3b>(i,j)[1],0);
+            }
+            else if(i > image.rows/2 && j > image.cols/2){
+                image.at<cv::Vec3b>(i,j) = Vec3b(0,0,image.at<cv::Vec3b>(i,j)[1]);
+            }
+        }
+    }
     imshow("Window", image);
     waitKey(0);
     return 0;
